3537adec4a62b8c43d10279337b8f061b9879a0c
Maarten van den Broek <m.j.vandenbroek@tudelft.nl>
2022-04-19 17:49:19 +0200

    add state vector pack / unpack to vw

--- vortexwake.py
+++ vortexwake.py
@@ -1,1 +1,60 @@
+import numpy as np
 import json
+
+
+class VortexWake:
+
+    def __init__(self, config_file):
+        with open(config_file, "r") as cf:
+            config = json.load(cf)
+
+            self.dim = config["dimension"]
+            self.num_elements = config.get("num_elements", 1)
+
+            self.num_rings = config["num_rings"]
+            self.num_points = self.num_elements + 1
+            self.num_controls = 2
+            self.num_turbines = config.get("num_turbines", 1)
+
+            self.total_rings = self.num_rings * self.num_turbines
+            self.total_points = self.num_points * self.total_rings
+            self.total_elements = self.num_elements * self.total_rings
+            self.total_controls = self.num_controls * self.num_turbines
+            self.num_states = (self.dim * self.total_points * 2) + self.total_elements + self.total_controls
+
+
+            self.time_step = config["time_step"]
+
+            self.X_index_start = 0
+            self.X_index_end = self.dim * self.total_points
+            self.G_index_start = self.X_index_end
+            # todo: reformulate such that single Gamma per ring
+            self.G_index_end = self.G_index_start + self.total_rings
+            self.U_index_start = self.G_index_end
+            self.U_index_end = self.U_index_start + self.dim * self.total_points
+            self.M_index_start = self.U_index_end
+            self.M_index_end = self.M_index_start + self.num_controls * self.num_turbines
+
+    def states_from_state_vector(self, q):
+        X = q[self.X_index_start: self.X_index_end].reshape(self.total_rings, self.num_points, self.dim)
+        G = q[self.G_index_start: self.G_index_end].reshape(self.total_rings, 1)
+        U = q[self.U_index_start: self.U_index_end].reshape(self.total_rings, self.num_points, self.dim)
+        M = q[self.M_index_start: self.M_index_end].reshape(self.num_controls, 1)
+        return X, G, U, M
+
+    def state_vector_from_states(self, X, G, U, M):
+        q = np.zeros((self.num_states, 1))
+        q[self.X_index_start:self.X_index_end:self.dim, 0] = X[:, :, 0].ravel()
+        q[self.X_index_start + 1:self.X_index_end:self.dim, 0] = X[:, :, 1].ravel()
+        if self.dim == 3:
+            q[self.X_index_start + 2:self.X_index_end:self.dim, 0] = X[:, :, 2].ravel()
+
+        q[self.G_index_start:self.G_index_end, 0] = G.ravel()
+
+        q[self.U_index_start:self.U_index_end:self.dim, 0] = U[:, :, 0].ravel()
+        q[self.U_index_start + 1:self.U_index_end:self.dim, 0] = U[:, :, 1].ravel()
+        if self.dim == 3:
+            q[self.U_index_start + 2:self.U_index_end:self.dim, 0] = U[:, :, 2].ravel()
+
+        q[self.M_index_start:self.M_index_end, 0] = M.ravel()
+        return q
