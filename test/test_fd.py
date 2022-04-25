92205f41c228e214f414a29bedc1202bb2011cb7
Maarten J. van den Broek <m.j.vandenbroek@tudelft.nl>
2022-04-25 14:31:00 +0200

    write full gradient test

--- test/test_fd.py
+++ test/test_fd.py
@@ -6,6 +6,8 @@ import json
 import matplotlib.pyplot as plt
 from functools import partial
 
+def get_colours(n):
+    return plt.cm.viridis(np.linspace(0.1, 0.9, n))
 
 def mean_absolute_error(A, B):
     return np.sum(np.abs(A - B)) / (np.product(A.shape))  # / np.sum(np.abs(A))
@@ -73,32 +75,36 @@ class TestDerivatives(unittest.TestCase):
         df_dq_B = df_dq_B.reshape(-1, self.fvw.num_states)
         df_dm_B = df_dm_B.reshape(-1, self.fvw.total_controls)
 
-        fig, ax = plt.subplots(1, 3, sharex='all', sharey='all', figsize=(12, 6))
-        vmax = 1e-3
-        if df_dq_A.shape[0] < 5:
-            ax[0].plot(df_dq_B.T)
-            ax[1].plot(df_dq_A.T)
-            ax[2].plot(df_dq_A.T - df_dq_B.T)
-        else:
-            ax[0].imshow(df_dq_B, cmap="RdBu", vmin=-vmax, vmax=vmax)
-            ax[1].imshow(df_dq_A, cmap="RdBu", vmin=-vmax, vmax=vmax)
-            ax[2].imshow(df_dq_A - df_dq_B, cmap="RdBu", vmin=-vmax, vmax=vmax)
-        ax[0].set_title("analytical")
-        ax[1].set_title("central diff")
-        ax[2].set_title("difference")
-        fig.savefig("./figures/{:d}d/adj_fd_{:s}_dq.png".format(self.dimension, name), format="png", dpi=600)
-
-        fig, ax = plt.subplots(1, 3, sharex='all', sharey='all', figsize=(12, 6))
-        vmax = 1e-3
-        ax[0].plot(df_dm_B)
-        ax[1].plot(df_dm_A)
-        ax[2].plot(df_dm_A - df_dm_B)
-        ax[0].set_title("analytical")
-        ax[1].set_title("central diff")
-        ax[2].set_title("difference")
-        fig.savefig("./figures/{:d}d/adj_fd_{:s}_dm.png".format(self.dimension, name), format="png", dpi=600)
+        self.print_difference_plots(df_dq_A, df_dq_B, name+"_dq")
+        self.print_difference_plots(df_dm_A, df_dm_B, name + "_dm")
 
         return (df_dq_A, df_dm_A), (df_dq_B, df_dm_B)
+
+    def print_difference_plots(self, A, B, name):
+        fig, ax = plt.subplots(1, 3, sharex='all', sharey='all', figsize=(12, 6))
+        vmax = 1e-3
+
+        if A.shape[1] < 5 or A.shape[0]< 5:
+            if A.shape[0] <A.shape[1]:
+                A = A.T
+                B = B.T
+            num_lines = A.shape[1]
+            colours = get_colours(num_lines+1)
+            for n in range(num_lines):
+                ax[0].plot(B[:,n], c=colours[n])
+                ax[1].plot(A[:,n], c=colours[n])
+                ax[2].plot((A-B)[:,n], c=colours[n])
+            for a in ax:
+                a.grid(True)
+        else:
+            ax[0].imshow(B, cmap="RdBu", vmin=-vmax, vmax=vmax)
+            ax[1].imshow(A, cmap="RdBu", vmin=-vmax, vmax=vmax)
+            ax[2].imshow(A-B, cmap="RdBu", vmin=-vmax, vmax=vmax)
+        ax[0].set_title("analytical")
+        ax[1].set_title("central diff")
+        ax[2].set_title("difference")
+        fig.savefig("./figures/{:d}d/derivative_difference_{:s}.png".format(self.dimension, name), format="png", dpi=600)
+
 
     @unittest.skip
     def test_new_rings(self):
@@ -108,7 +114,8 @@ class TestDerivatives(unittest.TestCase):
     def test_disc_velocity(self):
         # def disc_velocity(self, states, controls, with_tangent, all_turbines=False):
         function = partial(self.fvw.disc_velocity, all_turbines=True)
-        (df_dq_A, df_dm_A), (df_dq_B, df_dm_B) = self.print_graphical_derivative_comparison(function, self.q0, self.m0, "disc_velocity")
+        (df_dq_A, df_dm_A), (df_dq_B, df_dm_B) = self.print_graphical_derivative_comparison(function, self.q0, self.m0,
+                                                                                            "disc_velocity")
 
         threshold = 1e-6
         self.assertLess(mean_absolute_error(df_dq_A, df_dq_B), threshold)
@@ -120,7 +127,8 @@ class TestDerivatives(unittest.TestCase):
         points = self.rng.standard_normal((n, self.dimension))
         points[:, 0] += np.arange(n)
         function = partial(self.fvw.velocity, points=points)
-        (df_dq_A, df_dm_A), (df_dq_B, df_dm_B) = self.print_graphical_derivative_comparison(function, self.q0, self.m0, "velocity")
+        (df_dq_A, df_dm_A), (df_dq_B, df_dm_B) = self.print_graphical_derivative_comparison(function, self.q0, self.m0,
+                                                                                            "velocity")
 
         threshold = 1e-6
         self.assertLess(mean_absolute_error(df_dq_A, df_dq_B), threshold)
@@ -128,7 +136,8 @@ class TestDerivatives(unittest.TestCase):
 
     def test_power(self):
         function = self.fvw.calculate_power  # (q, m, with_tangent=True)
-        (df_dq_A, df_dm_A), (df_dq_B, df_dm_B) = self.print_graphical_derivative_comparison(function, self.q0, self.m0, "power")
+        (df_dq_A, df_dm_A), (df_dq_B, df_dm_B) = self.print_graphical_derivative_comparison(function, self.q0, self.m0,
+                                                                                            "power")
         threshold = 1e-6
         self.assertLess(mean_absolute_error(df_dq_A, df_dq_B), threshold)
         self.assertLess(mean_absolute_error(df_dm_A, df_dm_B), threshold)
@@ -137,27 +146,67 @@ class TestDerivatives(unittest.TestCase):
     def test_update_state(self):
         # inflow = self.fvw.unit_vector_x
         function = partial(self.fvw.update_state, inflow=self.fvw.unit_vector_x)
-        (df_dq_A, df_dm_A), (df_dq_B, df_dm_B) = self.print_graphical_derivative_comparison(function, self.q0, self.m0, "update_state")
+        (df_dq_A, df_dm_A), (df_dq_B, df_dm_B) = self.print_graphical_derivative_comparison(function, self.q0, self.m0,
+                                                                                            "update_state")
 
         indices = [self.fvw.X_index_start, self.fvw.G_index_start, self.fvw.U_index_start, self.fvw.M_index_start, -1]
-        for row in range(len(indices)-1):
+        for row in range(len(indices) - 1):
             ida0 = indices[row]
-            ida1 = indices[row+1]
-            for col in range(len(indices)-1):
+            ida1 = indices[row + 1]
+            for col in range(len(indices) - 1):
                 idb0 = indices[col]
-                idb1 = indices[col+1]
+                idb1 = indices[col + 1]
                 if row < 1 and col < 2:
                     threshold = 1e-3
                 else:
                     threshold = 1e-6
                 # print(row,col)
-                self.assertLess(mean_absolute_error(df_dq_A[ida0:ida1,idb0:idb1], df_dq_B[ida0:ida1,idb0:idb1]), threshold)
+                self.assertLess(mean_absolute_error(df_dq_A[ida0:ida1, idb0:idb1], df_dq_B[ida0:ida1, idb0:idb1]),
+                                threshold)
         threshold = 1e-5
         self.assertLess(mean_absolute_error(df_dm_A, df_dm_B), threshold)
 
-    @unittest.skip
+    # @unittest.skip
     def test_full_gradient(self):
-        assert False
+        num_steps = self.rng.integers(5,10)
+        n = num_steps
+        m = np.zeros((n+1, self.fvw.total_controls))
+        m[:, self.fvw.induction_idx::self.fvw.num_controls] = 0.25 + 0.02 * self.rng.random((n+1, self.fvw.num_controls))
+        m[:, self.fvw.yaw_idx::self.fvw.num_controls] = 20 + 2 * self.rng.random((n+1, self.fvw.num_controls))
+        u = np.zeros((n, self.fvw.dim)) + self.fvw.unit_vector_x
+        qh, dqn_dq, dqn_dm = self.fvw.run_forward(self.q0, m, u, n, with_tangent=True)
+        Q = self.rng.random((n+1, 1, self.fvw.total_turbines))
+        R = self.rng.random((n+1, self.fvw.total_controls, self.fvw.total_controls))
+        for k in range(n+1):
+            Q[k] = np.ones_like(Q[k])
+            R[k] = 0 #np.eye(self.fvw.total_controls)
+        phi, dphi_dq, dphi_dm = self.fvw.evaluate_objective_function(qh, m, Q, R, with_tangent=True)
+        gradient_B = vw.construct_gradient(dqn_dq, dqn_dm, dphi_dq, dphi_dm)
+
+        y0 = np.sum(phi)
+
+        y1 = np.zeros((num_steps, self.fvw.total_controls))
+        y2 = np.zeros((num_steps, self.fvw.total_controls))
+
+        m0 = m.copy()
+        dm = 1e-4
+        for c in range(self.fvw.total_controls):
+            for s in range(num_steps):
+                m = m0.copy()
+                m[s, c] -= dm
+                qh, dqn_dq, dqn_dm = self.fvw.run_forward(self.q0, m, u, n, with_tangent=False)
+                y1[s, c] = np.sum(self.fvw.evaluate_objective_function(qh, m, Q, R, with_tangent=False)[0])
+
+                m = m0.copy()
+                m[s, c] += dm
+                qh, dqn_dq, dqn_dm = self.fvw.run_forward(self.q0, m, u, n, with_tangent=False)
+                y2[s, c] = np.sum(self.fvw.evaluate_objective_function(qh, m, Q, R, with_tangent=False)[0])
+
+        gradient_A = (y2 - y1) / (2 * dm)
+
+        self.print_difference_plots(gradient_A, gradient_B, "full_gradient")
+
+        self.assertLess(mean_absolute_error(gradient_A, gradient_B),1e-5)
 
 
 class TestDerivatives3D(TestDerivatives):
@@ -178,6 +227,12 @@ class TestDerivatives3D(TestDerivatives):
 
     def test_update_state(self):
         super().test_update_state()
+
+    def test_power(self):
+        super().test_power()
+
+    def test_full_gradient(self):
+        super().test_full_gradient()
 
 
 class TestDerivatives2D(TestDerivatives):
@@ -200,3 +255,6 @@ class TestDerivatives2D(TestDerivatives):
 
     def test_power(self):
         super().test_power()
+
+    def test_full_gradient(self):
+        super().test_full_gradient()
