7c65e1f2a2897b5a98f41d1897c47144a5502166
Maarten J. van den Broek <m.j.vandenbroek@tudelft.nl>
2022-04-25 16:57:28 +0200

    add test for loading config

--- test/test_adam.py
+++ test/test_adam.py
@@ -3,6 +3,9 @@ from adam import *
 
 class TestAdam(unittest.TestCase):
 
+    def setUp(self):
+        self.rng = np.random.default_rng()
+
     def test_default_initialisation(self):
         adam = Adam()
         self.assertEqual(adam.alpha, 1e-3)
@@ -10,6 +13,25 @@ class TestAdam(unittest.TestCase):
         self.assertEqual(adam.beta_2, 0.999)
         self.assertEqual(adam.eps, 1e-8)
         self.assertEqual(adam.max_iter, 10)
+        self.assertEqual(adam.xt, 0)
+        self.assertEqual(adam.mt, 0)
+        self.assertEqual(adam.vt, 0)
+        self.assertEqual(adam.t, 0)
+        self.assertEqual(adam.f, 0)
+
+    def test_init_from_config(self):
+        config = {}
+        config["alpha"] = 1e-2*self.rng.random()
+        config["beta_1"] = self.rng.random()
+        config["beta_2"] = self.rng.random()
+        config["eps"] = 1-4*self.rng.random()
+        config["max_iter"] = self.rng.integers(0,300)
+        adam = Adam(config)
+        self.assertEqual(adam.alpha, config["alpha"])
+        self.assertEqual(adam.beta_1, config["beta_1"])
+        self.assertEqual(adam.beta_2, config["beta_2"])
+        self.assertEqual(adam.eps, config["eps"])
+        self.assertEqual(adam.max_iter, config["max_iter"])
         self.assertEqual(adam.xt, 0)
         self.assertEqual(adam.mt, 0)
         self.assertEqual(adam.vt, 0)
