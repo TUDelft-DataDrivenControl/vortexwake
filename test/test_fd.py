fix test broken in 2d gradient update, add du_dpt to 3d velocity

test/test_fd.py
@@ -6,6 +6,7 @@ import json
 import matplotlib.pyplot as plt
 from functools import partial
 
+
 def construct_jacobian_fd(f, q, m, dq=1e-4, dm=1e-4):
     q0 = q.copy()
     m0 = m.copy().ravel()
@@ -59,14 +60,13 @@ class TestDerivatives(unittest.TestCase):
         return qh[-1].copy(), m[-1].copy()
 
     def print_graphical_derivative_comparison(self, function, state, controls, name):
-
-        def f(q,m):
-            return function(q.copy(), m.copy(), with_tangent=False)[0].reshape(-1,1)
+        def f(q, m):
+            return function(q.copy(), m.copy(), with_tangent=False)[0].reshape(-1, 1)
 
         df_dq_A, df_dm_A = construct_jacobian_fd(f, state.copy(), controls.copy())
         df_dq_B, df_dm_B = function(state.copy(), controls.copy(), with_tangent=True)[1:3]
 
-        df_dq_B = df_dq_B.reshape(-1,self.fvw.num_states)
+        df_dq_B = df_dq_B.reshape(-1, self.fvw.num_states)
         df_dm_B = df_dm_B.reshape(-1, self.fvw.total_controls)
 
         fig, ax = plt.subplots(1, 3, sharex='all', sharey='all', figsize=(12, 6))
@@ -89,7 +89,6 @@ class TestDerivatives(unittest.TestCase):
         ax[2].set_title("difference")
         fig.savefig("./figures/adj_fd_{:s}_dm_{:d}.png".format(name, self.dimension), format="png", dpi=600)
 
-
     @unittest.skip
     def test_new_rings(self):
         assert False
@@ -102,9 +101,9 @@ class TestDerivatives(unittest.TestCase):
 
     # @unittest.skip
     def test_velocity(self):
-        n = self.rng.integers(5,10)
+        n = self.rng.integers(5, 10)
         points = self.rng.standard_normal((n, self.dimension))
-        points[:,0] += np.arange(n)
+        points[:, 0] += np.arange(n)
         function = partial(self.fvw.velocity, points=points)
         self.print_graphical_derivative_comparison(function, self.q0, self.m0, "velocity")
         # assert False
@@ -112,7 +111,6 @@ class TestDerivatives(unittest.TestCase):
     @unittest.skip
     def test_power(self):
         assert False
-
 
     # @unittest.skip
     def test_update_state(self):
@@ -133,6 +131,17 @@ class TestDerivatives3D(TestDerivatives):
         self.dimension = 3
         self.fvw = self.vw("../config/base_3d.json")
         self.q0, self.m0 = self.run_transient()
+
+    @unittest.skip
+    def test_velocity(self):
+        super().test_velocity()
+
+    def test_disc_velocity(self):
+        super().test_disc_velocity()
+
+    def test_update_state(self):
+        super().test_update_state()
+
 
 class TestDerivatives2D(TestDerivatives):
 

vortexwake.py
@@ -718,7 +718,7 @@ class VortexWake2D(VortexWake):
 
         dqn_dm[self.M_index_start:self.M_index_end] = dM0_dm
 
-        dqn_dm[self.G_index_start:self.G_index_end:2*self.num_rings] = dGamma0_dm 
+        dqn_dm[self.G_index_start:self.G_index_end:2*self.num_rings] = dGamma0_dm
         dqn_dm[self.G_index_start+1:self.G_index_end:2 * self.num_rings] = dGamma0_dm * -1
 
 
@@ -878,6 +878,7 @@ class VortexWake3D(VortexWake):
 
         n_p = p.shape[0]
         du_dq = np.zeros((n_p * 3, self.num_states))
+        du_dp = np.zeros((n_p * 3, n_p * 3))
         if with_tangent:
             ## select subset of full jacobian
             du_dX = du_dq[:, self.X_index_start:self.X_index_end]
@@ -1234,9 +1235,21 @@ class VortexWake3D(VortexWake):
 
             du_dq = np.where(np.isnan(du_dq), 0, du_dq)
 
+            du_dp[0::3, 0::3] = np.diag(np.sum(du_x_dx0_x, axis=0))
+            du_dp[0::3, 1::3] = np.diag(np.sum(du_x_dx0_y, axis=0))
+            du_dp[0::3, 2::3] = np.diag(np.sum(du_x_dx0_z, axis=0))
+
+            du_dp[1::3, 0::3] = np.diag(np.sum(du_y_dx0_x, axis=0))
+            du_dp[1::3, 1::3] = np.diag(np.sum(du_y_dx0_y, axis=0))
+            du_dp[1::3, 2::3] = np.diag(np.sum(du_y_dx0_z, axis=0))
+
+            du_dp[2::3, 0::3] = np.diag(np.sum(du_z_dx0_x, axis=0))
+            du_dp[2::3, 1::3] = np.diag(np.sum(du_z_dx0_y, axis=0))
+            du_dp[2::3, 2::3] = np.diag(np.sum(du_z_dx0_z, axis=0))
+
         du_dm = np.zeros((n_p * 3, len(controls)))
 
-        return result, du_dq, du_dm
+        return result, du_dq, du_dm, du_dp
 
     def update_state(self, states, controls, inflow, with_tangent):
         states = states.copy()